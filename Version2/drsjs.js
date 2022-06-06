c20150eaf00c0f08c1a80fb79595e8170e891459
Farukh Khassan Khojayev <farukh022198@gmail.com>
2022-06-06 14:45:32 -0400

    1. Implomented functions from validatetRightSingleLegReleves() to validateBolt() that's checks weather the input value is correct.

--- Version2/drsjs.js
+++ Version2/drsjs.js
@@ -267,9 +267,194 @@ function validateLastName() {
     alert("Invalid Character, Please enter only letters!");
     return (false);
 }
+/**  
+ * Functions from validatetRightSingleLegReleves() to validateBolt() checks weather the input value is correct. 
+ * If the input value positive it will return true. Otherwise it will alert the user and return false.  
+ */
 
+function validatetRightSingleLegReleves() {
+    var rightSingleLegReleves = DRSForm.rightSingleLegReleves.value;
+    if (rightSingleLegReleves >= 0) {
+        return (true);
+    }
 
+    alert("Invalid number, Right Single Leg Releves must be positive number!");
+    return (false);
+}
+
+function validateLeftSingleLegReleves() {
+    var leftSingleLegReleves = DRSForm.leftSingleLegReleves.value;
+    if (leftSingleLegReleves >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Left Single Leg Releves must be positive number!");
+    return (false);
+}
+
+function validatePlank() {
+    var plank = DRSForm.plank.value;
+    if (plank >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Plank time must be positive number!");
+    return (false);
+}
+
+function validateRightSidePlank() {
+    var rightSidePlank = DRSForm.rightSidePlank.value;
+    if (rightSidePlank >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Right Side Plank with Abduction(s) must be positive number!");
+    return (false);
+}
+
+function validateLeftSidePlank() {
+    var leftSidePlank = DRSForm.leftSidePlank.value;
+    if (leftSidePlank >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Left Side Plank with Abduction(s) must be positive number!");
+    return (false);
+}
+
+function validateRightSingleLegBridges() {
+    var rightSingleLegBridges = DRSForm.rightSingleLegBridges.value;
+    if (rightSingleLegBridges >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Right Single Leg Bridges must be positive number!");
+    return (false);
+}
+
+function validateLeftSingleLegBridges() {
+    var leftSingleLegBridges = DRSForm.leftSingleLegBridges.value;
+    if (leftSingleLegBridges >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Left Single Leg Bridges must be positive positive!");
+    return (false);
+}
+
+function validateRightHopTest1() {
+    var rightHopTest1 = DRSForm.rightHopTest1.value;
+    if (rightHopTest1 >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Right Side Hop Test1(cm) must be positive number!");
+    return (false);
+}
+
+function validateLeftHopTest1() {
+    var leftHopTest1 = DRSForm.leftHopTest1.value;
+    if (leftHopTest1 >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Left Side Hop Test1(cm) must be positive number!");
+    return (false);
+}
+
+function validateRightHopTest2() {
+    var rightHopTest2 = DRSForm.rightHopTest2.value;
+    if (rightHopTest2 >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Right Hop Test2(cm) must be positive number!");
+    return (false);
+}
+
+function validateLeftHopTest2() {
+    var leftHopTest2 = DRSForm.leftHopTest2.value;
+    if (leftHopTest2 >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Left Hop Test2(cm) must be positive!");
+    return (false);
+}
+
+function validateRightPasseReleveBalance() {
+    var rightPasseReleveBalance = DRSForm.rightPasseReleveBalance.value;
+    if (rightPasseReleveBalance >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Right Passe Releve Balance time(s) must be positive number!");
+    return (false);
+}
+
+function validateLeftPasseReleveBalance() {
+    var leftPasseReleveBalance = DRSForm.leftPasseReleveBalance.value;
+    if (leftPasseReleveBalance >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Left Passe Releve Balance time(s) must be positive number!");
+    return (false);
+}
+
+function validateRightPasseFlatFootBalance() {
+    var rightPasseFlatFootBalance = DRSForm.rightPasseFlatFootBalance.value;
+    if (rightPasseFlatFootBalance >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Right Passe Flat Foot Balance time(s) must be positive number!");
+    return (false);
+}
+
+function validateLeftPasseFlatFootBalance() {
+    var leftPasseFlatFootBalance = DRSForm.leftPasseFlatFootBalance.value;
+    if (leftPasseFlatFootBalance >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, Left Passe Flat Foot Balance time(s) must be positive number!");
+    return (false);
+}
+
+function validateCkcuest() {
+    var ckcuest = DRSForm.ckcuest.value;
+    if (ckcuest >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, CKCUEST must be positive number!");
+    return (false);
+}
+
+function validateBolt() {
+    var bolt = DRSForm.bolt.value;
+    if (bolt >= 0) {
+        return (true);
+    }
+
+    alert("Invalid number, BOLT test score must be positive number!");
+    return (false);
+}
+
+/**
+ * This function checks weather input value is correct for all input fields. 
+ * If the all input filds are true, it will create pdf, otherwise it will alert the user. 
+ */
 function validateForm() {
+
+    var dateInput = document.getElementById("testingdate");
+    console.log(dateInput.toString());
+    if (!dateInput.value) {
+        alert("Date must be filled out");
+        return false;
+    };
+
     if (!validateEmail()) { return false; }
     var email = document.getElementById("email").value;
     if (email === "") {
@@ -297,6 +482,9 @@ function validateForm() {
         alert("Company must be filled out");
         return false;
     };
+    if (!validatetRightSingleLegReleves()) {
+        return false;
+    }
     var rightSingleLegReleves = document.getElementById("rightSingleLegReleves").value;
     if (rightSingleLegReleves === "") {
         alert("Right single leg releves must be filled out");
@@ -307,74 +495,119 @@ function validateForm() {
         alert("Left single leg releves must be filled out");
         return false;
     };
+    if (!validateLeftSingleLegReleves()) {
+        return false;
+    }
     var plank = document.getElementById("plank").value;
     if (plank === "") {
         alert("Plank must be filled out");
         return false;
     };
+    if (!validatePlank()) {
+        return false;
+    }
     var rightSidePlank = document.getElementById("rightSidePlank").value;
     if (rightSidePlank === "") {
         alert("Right side plank must be filled out");
         return false;
     };
+    if (!validateRightSidePlank()) {
+        return false;
+    }
     var leftSidePlank = document.getElementById("leftSidePlank").value;
     if (leftSidePlank === "") {
         alert("Left side plank must be filled out");
         return false;
     };
+    if (!validateLeftSidePlank()) {
+        return false;
+    }
     var rightSingleLegBridges = document.getElementById("rightSingleLegBridges").value;
     if (rightSingleLegBridges === "") {
         alert("Right single leg bridges must be filled out");
         return false;
     };
+    if (!validateRightSingleLegBridges()) {
+        return false;
+    }
     var leftSingleLegBridges = document.getElementById("leftSingleLegBridges").value;
     if (leftSingleLegBridges === "") {
         alert("Left single leg bridges must be filled out");
         return false;
     };
+    if (!validateLeftSingleLegBridges()) {
+        return false;
+    }
     var rightHopTest1 = document.getElementById("rightHopTest1").value;
     if (rightHopTest1 === "") {
         alert("Right hop test 1 must be filled out");
         return false;
     };
+    if (!validateRightHopTest1()) {
+        return false;
+    }
     var leftHopTest1 = document.getElementById("leftHopTest1").value;
     if (leftHopTest1 === "") {
         alert("Left hop test 1 must be filled out");
         return false;
     };
+    if (!validateLeftHopTest1()) {
+        return false;
+    }
     var rightHopTest2 = document.getElementById("rightHopTest2").value;
     if (rightHopTest2 === "") {
         alert("Right hop test 2 must be filled out");
         return false;
     };
+    if (!validateRightHopTest2()) {
+        return false;
+    }
     var leftHopTest2 = document.getElementById("leftHopTest2").value;
     if (leftHopTest2 === "") {
         alert("Left hop test 2 must be filled out");
         return false;
     };
+    if (!validateLeftHopTest2()) {
+        return false;
+    }
     var rightPasseReleveBalance = document.getElementById("rightPasseReleveBalance").value;
     if (rightPasseReleveBalance === "") {
         alert("Right passe releve balance must be filled out");
         return false;
     };
+    if (!validateRightPasseReleveBalance()) {
+        return false;
+    }
     var leftPasseReleveBalance = document.getElementById("leftPasseReleveBalance").value;
     if (leftPasseReleveBalance === "") {
         alert("Left passe releve balance must be filled out");
         return false;
     };
+    if (!validateLeftPasseReleveBalance()) {
+        return false;
+    }
     var rightPasseFlatFootBalance = document.getElementById("rightPasseFlatFootBalance").value;
     if (rightPasseFlatFootBalance === "") {
         alert("Right passe flat foot balance must be filled out");
         return false;
     };
+    if (!validateRightPasseFlatFootBalance()) {
+        return false;
+    }
     var leftPasseFlatFootBalance = document.getElementById("leftPasseFlatFootBalance").value;
     if (leftPasseFlatFootBalance === "") {
         alert("Left passe flat foot balance must be filled out");
         return false;
     };
+    if (!validateLeftPasseFlatFootBalance()) {
+        return false;
+    }
     var ckcuest = document.getElementById("ckcuest").value;
     if (ckcuest == "") {
         alert("CKCUEST must be filled out");
+        return false;
+    };
+    if (!validateCkcuest) {
         return false;
     }
     var bolt = document.getElementById("bolt").value;
@@ -382,11 +615,18 @@ function validateForm() {
         alert("Bolt score must be filled out");
         return false;
     };
+    if (!validateBolt()) {
+        return false;
+    }
+
+    pdfMake.createPdf(create_pdf()).print();
 
 }
 
 
 function create_pdf() {
+
+
     var d = getData()
 
     var dd = {
